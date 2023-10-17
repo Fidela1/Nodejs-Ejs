99e251f9877dc09b13b3c1e69d71b25acc7f2a20
TUYIZERE Fidela <fidelatuyizere3@gmail.com>
2023-10-17 17:19:24 +0200

    conect to database

--- main.js
+++ main.js
@@ -7,9 +7,35 @@ const sesssion = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) =>{
-   res.send("Hello world"); 
+
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+.then (() =>{
+  console.log('Connected to the dadatabase!')
 })
+
+//middlewares
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use(sesssion({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false,
+})
+)
+app.use((req, res, next) => {
+    req.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+// set template engine
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) =>{
+    res.send("Hello world"); 
+ })
 
 app.listen(PORT, () =>{
     console.log(`server started at http://localhost:${PORT}`);
